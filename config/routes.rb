0b9cbbf72ec009dfb775111cd50d3ef6617169fe
WalkerTexas-coder <austinsrwalker@gmail.com>
2023-02-03 03:12:50 -0800

    active cable drawing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Mount the action cable server
   mount ActionCable.server => '/cable'
-  root 'pages#home'
   get 'draw', to: 'pages#draw'
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
